7567567db210d609a8dddb13c0ba3bb702cd9843
Bolter87 <workw6132@gmail.com>
2021-10-15 12:00:42 +0200

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PatreonComponent } from './patreon/patreon.component';
+import { CommissionComponent } from './commission/commission.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    GalleryComponent,
+    PatreonComponent,
+    CommissionComponent,
+    SocialMediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
